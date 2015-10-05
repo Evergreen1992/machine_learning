58a315431a78e273d65a44a68b983ab7eb1ba125
Evergreen1992 <781804951@qq.com>
2015-10-05 16:02:20 +0800

    决策树.

---
+++ trees/trees.py
@@ -0,0 +1,89 @@
+#encoding:utf-8
+from math import log
+import operator
+
+#创建决策树
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    """类别完全相同则停止划分"""
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:#全部划分结束.选择出现频率最高的.
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+#求classList中出现频率最高的元素
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0 
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+#选择最好的特征值划分数据集
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1#特征值的数量
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):#遍历特征值
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:#计算每个特征值的划分集的香农熵
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if( infoGain > bestInfoGain):#计算信息增益
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+def createDataSet():
+    dataSet = [[1,1,'yes'],[1,1,'yes'],[1,0,'no'],[0,1,'no'],[0,1,'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+#划分数据集(数据集,数据集特征,特征返回值)
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value :
+            reduceFeatVec = featVec[:axis]
+            reduceFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reduceFeatVec)
+    return retDataSet
+
+#计算数据集的香农熵
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+"""
+print calcShannonEnt(data)
+print splitDataSet(data, 0, 1)
+"""
+data, labels = createDataSet()
+tree = createTree(data, labels)
+print tree
