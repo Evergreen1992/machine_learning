f9be5c9a1be007d3917a02c5f6c6b76fa4c4aef6
Evergreen1992 <781804951@qq.com>
2016-01-16 21:28:47 +0800

    matplotlib examples

---
+++ toolkit/matplotlibExamples.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+####################
+#   matplotlib例子      #
+####################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.plot([0,1],[2,3])#画直线
+plt.title("test")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
