4438c93b7fa5698d162490d3b7e9ad114f19c058
Evergreen1992 <781804951@qq.com>
2016-01-16 10:57:12 +0800

    ；）

---
+++ toolkit/matplotlib.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+####################
+#   matplotlib例子      #
+####################
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(8,4))
+plt.plot([1,2,3,4])
+plt.ylabel("y坐标轴")
+plt.xlabel("x坐标轴")
+plt.title("python绘图工具")
+plt.show()
