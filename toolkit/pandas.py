94f33d5fe70c1f036e2004272a812765e57e8cd6
Evergreen1992 <781804951@qq.com>
2016-01-16 20:18:15 +0800

    pandas examples

---
+++ toolkit/pandas.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+import pandas as pd
+import numpy as np
+from scipy import *
+
+reload(pd)
+m=np.mat([[1,2,3]])
+print m
+array=np.array([1,2,3,4,5,6])
+print array
+
+s=pd.Series([1,3,5,6,8])
+#print s
